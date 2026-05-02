086a50e8f7d6d5e8b82a21c077e4f1449ef49775
tyw <tyw66_git@qq.com>
2026-05-02 23:08:52 +0800

    完成Lesson02时间系统

--- Lesson02/cube_move.py
+++ Lesson02/cube_move.py
@@ -1,43 +1,70 @@
 '''
-学习时间系统
-'''
+学习时间系统——方块的移动
+通过引入delta_time，实现与帧率解耦
+限制帧率为 30、60、100，观察每秒总位移是否基本一致
+''' 
 import pygame
-
-WINDOW_SIZE = (640, 480)    # 窗口大小
+import sys
 
 def handle_input():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit()
+            sys.exit()
 
 
-def update():
-    pass
+def update(delta_time):
+    global cube_position, cube_velocity
+    cube_position[0] += cube_velocity[0] * delta_time
+    cube_position[1] += cube_velocity[1] * delta_time
+    if cube_position[0] < 0 or cube_position[0] + 100 > WINDOW_SIZE[0]:
+        cube_velocity[0] = -cube_velocity[0]
+    if cube_position[1] < 0 or cube_position[1] + 100 > WINDOW_SIZE[1]:
+        cube_velocity[1] = -cube_velocity[1]
 
-def render():
+
+def render(delta_time):
     # 渲染场景
     screen.fill((116, 161, 195))
+    # 渲染FPS
+    fps_text = font.render(f"FPS: {fps:.2f}", True, (255, 255, 255))
+    screen.blit(fps_text, (10, 50)) 
+    # 渲染动态时间
+    delta_time_text = font.render(f"Delta Time: {delta_time:.4f}", True, (255, 255, 255))
+    screen.blit(delta_time_text, (10, 100)) 
+    # 绘制矩形
+    pygame.draw.rect(screen, CUBE_COLOR, (cube_position[0], cube_position[1], 100, 100))   
+    # 更新显示
     pygame.display.flip()
 
 
 if __name__ == "__main__":  
-    # 初始化参数
+    # 初始化游戏参数    
+    WINDOW_SIZE = (800, 600)                # 窗口大小  
+    CUBE_COLOR = (255, 255, 255)             # 立方体颜色        
+    cube_position = [10, 240]               # 立方体初始位置
+    cube_velocity = [200, 0]                # 立方体初始速度  
+    fps = 60                                # 渲染帧率 <-----修改，观察效果
 
     # 初始化 Pygame
     pygame.init()
     screen = pygame.display.set_mode(WINDOW_SIZE)
-    pygame.display.set_caption("Lesson02 - Cube Move")
+    pygame.display.set_caption("Lesson02 - Time System")
+    font = pygame.font.Font(None, 30)
     clock = pygame.time.Clock()
-    font = pygame.font.Font(None, 36)
+    last_time = pygame.time.get_ticks() / 1000.0
 
     # 游戏主循环
-    while True:
+    while True: 
+        # 计算时间步长
+        current_time = pygame.time.get_ticks() / 1000.0
+        delta_time = current_time - last_time
+        last_time = current_time        
         # 处理输入
-        handle_input()
-        # 更新场景状态
-        update()
-        # 渲染场景
-        render()
-        # 控制帧率
-        clock.tick(60)
+        handle_input()        
+        # 执行固定时间步长的物理更新
+        update(delta_time)        
+        # 渲染更新
+        render(delta_time)        
+        # 限制帧率
+        clock.tick(fps) 
